cca20ed81eee090a2f54be218dcd626aca282e51
Nguyễn Qúy Hưng <161361528+S1zzX@users.noreply.github.com>
2026-02-01 19:47:31 +0700

    Delete start-dev.bat

--- start-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Convenience wrapper to run the PowerShell script from cmd or Explorer on Windows
-SETLOCAL
-SET script=%~dp0start-dev.ps1
-powershell -NoProfile -ExecutionPolicy Bypass -File "%script%"
-ENDLOCAL
